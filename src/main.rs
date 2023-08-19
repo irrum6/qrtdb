37e7f240d8f22205f980d12d02b52941b28358e0
irakli mindorashvili <irrum6@github.com>
2023-08-19 04:22:04 +0400

    clean main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
-// use qrtdb::qrtlib::help;
-// use qrtdb::{Database, MetaCommands};
-
 use qrtdb::qrtlib::rundb4;
-mod file_test_old;
 
-use qrtdb::qrtlib::field_types::DateFieldCustom;
-
-mod query;
+// mod query;
 fn main() {
-    // DateFieldCustom::test_sede();
     rundb4();
     return;
 }
